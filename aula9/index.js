8b1f499770bad0f4843607773237c63cc8c3a373
Daniel Silva <daniel.raizon@gmail.com>
2021-03-14 19:03:48 -0300

    Primitive types.

--- aula9/index.js
+++ aula9/index.js
@@ -1,4 +1,24 @@
-let nome = 'Daniel';
-var nome = 'Daniel2';
+const nome = 'Daniel1'; // 'string'
+const nome1 = 'Daniel2'; // "string"
+const nome2 = `Daniel3`; // `string`
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined
+const sobrenomeAluno = null; // nulo
+const aprovado = false; // Boolean
 
-console.log(nome)
+console.log(
+	typeof (nome, nome1, nome2),
+	typeof (num1, num2),
+	typeof nomeAluno,
+	typeof sobrenomeAluno,
+	typeof aprovado,
+	nome,
+	nome1,
+	nome2,
+	num1,
+	num2,
+	nomeAluno,
+	sobrenomeAluno,
+	aprovado,
+);
