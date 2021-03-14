481d259908514a6c2742dbf67637af8aacaec00c
Daniel Silva <daniel.raizon@gmail.com>
2021-03-13 22:56:36 -0300

    Let variables.

---
+++ aula6/index.js
@@ -0,0 +1,18 @@
+let nome;
+
+nome = 'Qualquer valor';
+console.log(nome);
+
+nome = 'Daniel';
+console.log(nome);
+
+// console.log(nome, 'nasceu em 1984.');
+// console.log('Em 2000', nome, 'conheceu Maria.');
+// console.log(nome, 'casou-se com Maria em 2012.');
+// console.log('Maria teve 1 filho com', nome, 'em 2015.');
+// console.log('O filho de', nome, 'se chama Eduardo.');
+
+let nomeCliente = 'Daniel';
+nomeCliente = 'Daniel2';
+
+console.log(nomeCliente);
