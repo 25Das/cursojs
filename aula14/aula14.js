05e7fa539211cefd5663e17d68b268d14866cae2
Daniel Silva <daniel.raizon@gmail.com>
2021-03-31 15:07:50 -0300

    More number examples.

---
+++ aula14/aula14.js
@@ -0,0 +1,44 @@
+// IEEE 754-2008
+let num1 = 0.7;
+const num2 = 0.1;
+
+// num1 = (num1 * 100 + num2 * 100) / 100;
+// num1 = (num1 * 100 + num2 * 100) / 100;
+// num1 = (num1 * 100 + num2 * 100) / 100;
+
+num1 = Number(num1.toFixed(2));
+
+console.log(num1.toFixed(2));
+
+// num1 *= 100;
+// num2 *= 100;
+
+// const soma = (num1 + num2) / 100;
+
+// console.log(soma);
+// console.log(Number.isInteger(soma));
+
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+
+// num1 = parseFloat(num1.toFixed(2));
+
+// console.log(num1.toString() + num2); // Temporário
+// num1 = num1.toString(); // Permanente
+// console.log(num1.toString(2)); // Binário
+// console.log(num1.toFixed(2)); // Casas decimais
+// console.log(Number.isInteger(num1)); // É inteiro = true ou false
+// console.log(Number.isNaN(temp));
+// const temp = num1 * '5';
+// console.log(temp);
