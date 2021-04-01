437ec4565fffced901a14187cf8ec1c350249a6f
Daniel Silva <daniel.raizon@gmail.com>
2021-04-01 14:15:43 -0300

    Basic of Arrays

---
+++ aula16/aula16.js
@@ -0,0 +1,31 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(typeof alunos);
+
+// console.log(alunos instanceof Array); // Verifica se é um Array
+
+// console.log(alunos.slice(0, -2)); // Fatiamento de Arrays
+
+// console.log(alunos[50]); // Acessa índices inexistentes e retorna undefined
+
+// delete alunos[1]; // Remove sem alterar os índices
+
+// const removido = alunos.shift(); // Remove do começo
+// const removido = alunos.pop(); // Remove do final
+
+// alunos.unshift('Luiza'); // Adiciona no começo
+// alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo'; // Altera
+// alunos[3] = 'Luiza';
+
+// console.log(alunos); // Acessa
+// console.log(alunos[0]);
+// console.log(alunos[2]);
