26f82313746df5a345a2a8ca30496d8d9f2884eb
Daniel Silva <daniel.raizon@gmail.com>
2021-04-01 12:33:07 -0300

    Math object.

---
+++ aula15/aula15.js
@@ -0,0 +1,15 @@
+console.log(!!(100 / 0));
+
+// const num1 = 25;
+// console.log(num1 ** 0.5);
+
+// const num1 = 9.49;
+// const num2 = Math.floor(num1);
+// const num2 = Math.ceil(num1);
+// const num2 = Math.round(num1);
+
+// console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
+// console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(Math.pow(2, 10));
+// console.log(2 ** 10);
