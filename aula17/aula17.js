01d9dfe743864ceecc64f54211390a9a31301b89
Daniel Silva <daniel.raizon@gmail.com>
2021-04-01 14:54:59 -0300

    Basic of functions.

---
+++ aula17/aula17.js
@@ -0,0 +1,17 @@
+// function soma(x = 1, y = 1) {
+// 	const resultado = x + y;
+// 	return resultado;
+// }
+
+// const resultado = soma(4, 2);
+// console.log(resultado);
+
+// const raizQ = function (n) {
+// 	return n ** 0.5;
+// };
+
+const raizQ = n => n ** 0.5;
+
+console.log(raizQ(9));
+console.log(raizQ(16));
+console.log(raizQ(25));
