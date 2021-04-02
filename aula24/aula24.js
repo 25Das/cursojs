d7471aa2cfe49261f2a6ad54ca2a370430d6c8eb
Daniel Silva <daniel.raizon@gmail.com>
2021-04-02 15:38:34 -0300

    If, else if and else statements.

---
+++ aula24/aula24.js
@@ -0,0 +1,23 @@
+//*
+// const hora = 1;
+
+// if (hora >= 0 && hora < 6) {
+// 	console.log('Boa noite');
+// } else if (hora >= 6 && hora < 12) {
+// 	console.log('Bom dia');
+// } else if (hora >= 12 && hora < 18) {
+// 	console.log('Boa tarde');
+// } else if (hora >= 18 && hora <= 23) {
+// 	console.log('Boa noite');
+// } else {
+// 	console.log('Hora inválida');
+// }
+
+//*
+const tenhoGrana = NaN; // Falsy
+
+if (tenhoGrana) {
+	console.log('Vou sair de casa');
+} else {
+	console.log('Não vou sair de casa');
+}
