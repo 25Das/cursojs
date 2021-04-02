85c0de4ee25350fa0d5a3783ef9faadff96dbcad
Daniel Silva <daniel.raizon@gmail.com>
2021-04-01 21:09:54 -0300

    Logical operators.

---
+++ aula22/aula22.js
@@ -0,0 +1,25 @@
+/** Operadores Lógicos
+ *  Operador && -> AND -> E
+ *  Operador || -> OR -> OU
+ *  Operador ! -> NOT -> NÃO
+ */
+
+//*
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || false || false;
+// const expressaoNot = !true;
+// const andOrNot = false || (!true && false);
+
+// console.log(expressaoAnd);
+// console.log(expressaoOr);
+// console.log(expressaoNot);
+// console.log(andOrNot);
+
+//*
+// const usuario = 'Daniel';
+// const senha = '123456';
+
+// const vaiLogar = usuario === 'Daniel' && senha === '12345';
+// console.log(vaiLogar);
+
+console.log(!true);
