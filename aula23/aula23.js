70f370087b2bdddce59f30720712e5156a16b187
Daniel Silva <daniel.raizon@gmail.com>
2021-04-01 21:33:23 -0300

    Short-Circuit Evaluation.

---
+++ aula23/aula23.js
@@ -0,0 +1,37 @@
+/**
+ * *Falsy
+ * false
+ * 0
+ * '' "" ``
+ * null / undefined
+ * NaN
+ */
+
+//*
+// console.log('Daniel Silva' && NaN && 'Fernanda');
+
+//*
+// function falaOi() {
+// 	return 'Oi';
+// }
+
+// const vaiExecutar = 'Joãozinho';
+
+// console.log(vaiExecutar && falaOi());
+
+//*
+// console.log(0 || false || null || 'Daniel' || true);
+
+//*
+// const corUsuario = 'vermelho';
+// const corPadrao = corUsuario || 'preto';
+
+// console.log(corPadrao);
+
+const a = 0;
+const b = null;
+const c = false;
+const d = false;
+const e = NaN;
+
+console.log(a || b || 'Joãozinho' || c || d || e);
