d3bfbacdf4486a5206be13ef2a36ae94631d50f9
Daniel Silva <daniel.raizon@gmail.com>
2021-04-04 18:51:00 -0300

    Classic ForLoop.

---
+++ aula34/aula34.js
@@ -0,0 +1,19 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+//*
+// for (let i = 0; i < 11; i++) {
+// 	const par =  i % 2 === 0 ? 'par' : 'ímpar'
+// 	console.log(i, par);
+// }
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Laranja', 'Banana', 'Abacaxi', 'Melância'];
+
+for (let i = 0; i < frutas.length; i++) {
+	console.log(`Índice ${i}`, frutas[i]);
+
+}
