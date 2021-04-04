d9528aa6bde22127af08d76eb049358928c70b29
Daniel Silva <daniel.raizon@gmail.com>
2021-04-04 00:03:27 -0300

    More differences between var and let/const .

---
+++ aula31/aula31.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-var */
+/* eslint-disable prefer-const */
+
+//* Let tem escopo de bloco { ... bloco }
+//* Var so tem escopo de função
+
+// const verdadeira = true;
+
+// let nome = 'Daniel'; // criando
+// var nome2 = 'Daniel'; // criando
+
+// if (verdadeira) {
+// 	let nome = 'Alves'; // criando
+// 	var nome2 = 'Fernanda'; // redeclarando
+
+// 	if (verdadeira) {
+// 		let nome = 'Outra coisa'; // criando
+// 		var nome2 = 'Santos'; // redeclarando
+// 	}
+// }
+// console.log(nome, nome2);
+
+//*
+// const verdadeira = true;
+
+// function falaOi() {
+// 	if (verdadeira) {
+// 		let nome = 'Daniel';
+// 		var sobrenome = 'Silva';
+// 	}
+// 	console.log(sobrenome);
+// }
+
+// falaOi();
+
+let sobrenome = 'Silva';
+console.log(sobrenome);
