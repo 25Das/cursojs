ec1baa0199a4b27ded714dfd96ba9e6611f3396e
Daniel Silva <daniel.raizon@gmail.com>
2021-06-06 00:10:02 -0300

    While and Do While examples.

---
+++ js2Logic/aula39/aula39.js
@@ -0,0 +1,35 @@
+function random(min, max) {
+	const r = Math.random() * (max - min) + min;
+	return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+
+let rand = 10; //random(min, max);
+
+while (rand !== 10) {
+	// rand = random(min, max);
+	console.log(rand);
+}
+
+console.log('#######');
+
+do {
+	// rand = random(min, max);
+	console.log(rand);
+} while (rand !== 10);
+
+
+
+
+// const nome = 'Daniel';
+
+// let i = 0;
+
+// while (i < nome.length) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// console.log('Segue a vida ...')
