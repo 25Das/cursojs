f4ff5b22e5b8e45ca419764d0270f279e3c335e7
Daniel Silva <daniel.raizon@gmail.com>
2021-06-07 02:09:07 -0300

    Timer with setInterval exercise.

---
+++ js2Logic/aula47/assets/js/main.js
@@ -0,0 +1,44 @@
+function clock() {
+	function criaHoraDosSegundos(segundos) {
+		const data = new Date(segundos * 1000);
+
+		return data.toLocaleTimeString('pt-BR', {
+			timeZone: 'UTC',
+		});
+	}
+
+	const relogio = document.querySelector('.relogio');
+	let segundos = 0;
+	let timer;
+
+	function iniciaRelogio() {
+		timer = setInterval(() => {
+			segundos += 1;
+			relogio.innerHTML = criaHoraDosSegundos(segundos);
+		}, 1000);
+	}
+
+	document.addEventListener('click', e => {
+		const el = e.target;
+
+		if (el.classList.contains('iniciar')) {
+			relogio.classList.remove('pausado');
+			clearInterval(timer);
+			iniciaRelogio();
+		}
+
+		if (el.classList.contains('pausar')) {
+			relogio.classList.add('pausado');
+			clearInterval(timer);
+		}
+
+		if (el.classList.contains('zerar')) {
+			relogio.classList.remove('pausado');
+			clearInterval(timer);
+			relogio.innerHTML = '00:00:00';
+			segundos = 0;
+		}
+	});
+}
+
+clock();
