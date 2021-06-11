ea564ba7cd6b194ee31894e9184fa567ea50cd05
Daniel Silva <daniel.raizon@gmail.com>
2021-06-11 05:55:02 -0300

    Review of basic of arrays.

---
+++ js3Functions/aula62/aula62.js
@@ -0,0 +1,11 @@
+// const nomes = ['Daniel', 'Fernanda', 'Riven', 'Jhin', 'Kayn'];
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+// const nome = 'Daniel Alves da Silva';
+// const nomes = nome.split(' ');
+
+const nomes = ['Daniel', 'Alves', 'da', 'Silva'];
+const nome = nomes.join(' ');
+
+console.log(nome);
