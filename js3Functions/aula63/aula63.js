3b91ff628b6aa977a5f25bdb519def4b8c0c88e2
Daniel Silva <daniel.raizon@gmail.com>
2021-06-20 17:35:51 -0300

    Array splice method.

---
+++ js3Functions/aula63/aula63.js
@@ -0,0 +1,12 @@
+const nomes = ['Daniel', 'Fernanda', 'Riven', 'Jhin', 'Kayn'];
+
+nomes.splice(2, 0, 'Ordis', 'Clem');
+console.log(nomes);
+
+// nomes.splice(índice atual, delete, el1, el2, el3);
+
+// pop
+// const removidos = nomes.splice(-1, 1);
+
+// shift
+// const removidos = nomes.splice(0, 1);
