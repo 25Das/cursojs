4d92b6dba321cc0b3e1724b2cce865c23193b411
Daniel Silva <daniel.raizon@gmail.com>
2021-08-30 20:22:48 -0300

    Configure Express to use JSON.

--- NodeJs/aula17/server.js
+++ NodeJs/aula17/server.js
@@ -29,6 +29,7 @@ mongoose
 app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
