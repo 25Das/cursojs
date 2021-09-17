f2d6203038b50b328961792c1f983fb415f48245
Daniel Silva <daniel.raizon@gmail.com>
2021-09-16 23:02:07 -0300

    Configuring and testing Sequelize.

--- apiRest/src/controllers/HomeController.js
+++ apiRest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-	index(req, res) {
-		res.json({
-			tudoCerto: true,
+	async index(req, res) {
+		const novoAluno = await Aluno.create({
+			nome: 'Fernanda',
+			sobrenome: 'Santos',
+			email: 'fernanda23021999@gmail.com',
+			idade: 22,
+			peso: 50,
+			altura: 1.53,
 		});
+		res.json(novoAluno);
 	}
 }
 
