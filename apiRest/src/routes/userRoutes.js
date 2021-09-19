1261937082944e409483fa32fc4495069e92330e
Daniel Silva <daniel.raizon@gmail.com>
2021-09-18 23:20:42 -0300

    Security fixes for users.

--- apiRest/src/routes/userRoutes.js
+++ apiRest/src/routes/userRoutes.js
@@ -5,10 +5,12 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
+// Não deveria existir
+// router.get('/', loginRequired, UserController.index); // Listar usuários
+// router.get('/:id', UserController.show); // Listar usuário
+
 router.post('/', UserController.store);
-router.get('/', loginRequired, UserController.index);
-router.get('/:id', UserController.show);
-router.put('/:id', UserController.update);
-router.delete('/:id', UserController.delete);
+router.put('/', loginRequired, UserController.update);
+router.delete('/', loginRequired, UserController.delete);
 
 export default router;
