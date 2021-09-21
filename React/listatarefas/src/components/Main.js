9144f8016e5abc198e0037ef892140660c51bfc6
Daniel Silva <daniel.raizon@gmail.com>
2021-09-21 03:18:32 -0300

    Styling the tasks.

--- React/listatarefas/src/components/Main.js
+++ React/listatarefas/src/components/Main.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 // Form
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
+
+// Tarefas
+// import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 class Main extends Component {
 	state = {
 		novaTarefa: '',
+		tarefas: ['Fazer café', 'Beber água', 'Estudar'],
 	};
 
 	handleChange = e => {
@@ -16,7 +21,7 @@ class Main extends Component {
 	};
 
 	render() {
-		const { novaTarefa } = this.state;
+		const { novaTarefa, tarefas } = this.state;
 
 		return (
 			<div className='main'>
@@ -28,6 +33,18 @@ class Main extends Component {
 						<FaPlus />
 					</button>
 				</form>
+
+				<ul className='tarefas'>
+					{tarefas.map(tarefa => (
+						<li key={tarefa}>
+							{tarefa}
+							<div>
+								<FaEdit className='edit' />
+								<FaWindowClose className='delete' />
+							</div>
+						</li>
+					))}
+				</ul>
 			</div>
 		);
 	}
