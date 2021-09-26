b546dbf3a87f76ed614771a2c473d7c426f0df41
Daniel Silva <daniel.raizon@gmail.com>
2021-09-26 09:00:38 -0300

    Use the basic of style-components.

---
+++ React/react-base/src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+	return (
+		<>
+			<Title>
+				Login
+				<small>Olá</small>
+			</Title>
+			<Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+		</>
+	);
+}
