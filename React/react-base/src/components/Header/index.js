2746c5a85cb482db2eb10fe3834e841da9134c56
Daniel Silva <daniel.raizon@gmail.com>
2021-09-26 09:48:15 -0300

    Create NavBar.

---
+++ React/react-base/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Nav } from './styled';
+
+export default function Header() {
+	return (
+		<Nav>
+			<a href='/'>Home</a>
+			<a href='/'>Teste</a>
+		</Nav>
+	);
+}
