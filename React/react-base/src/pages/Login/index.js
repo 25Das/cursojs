48c5c3ef076acfc0d9bcb2d8dc24debccfda6d3f
Daniel Silva <daniel.raizon@gmail.com>
2021-09-26 09:15:07 -0300

    Setup a GlobalStyle.

--- React/react-base/src/pages/Login/index.js
+++ React/react-base/src/pages/Login/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
 export default function Login() {
 	return (
 		<>
-			<Title>
-				Login
-				<small>Olá</small>
-			</Title>
-			<Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+			<Container>
+				<Title>
+					Login
+					<small>Olá</small>
+				</Title>
+
+				<Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+			</Container>
 		</>
 	);
 }
