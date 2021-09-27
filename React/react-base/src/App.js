614c5996777faef7e2b1c93ca2a3261116a8b970
Daniel Silva <daniel.raizon@gmail.com>
2021-09-27 04:25:42 -0300

    Remove toast lines because it is not in use right now.

--- React/react-base/src/App.js
+++ React/react-base/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
@@ -8,9 +8,6 @@ import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
-	toast.success('Sucesso!', { autoClose: 3000, toastId: 'success' });
-	toast.error('Deu ruim!', { autoClose: 3000, toastId: 'error' });
-
 	return (
 		<Router history={history}>
 			<Header />
