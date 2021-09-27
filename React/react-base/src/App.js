79391a5dfaf6d48bdaf006d0b4e2430fc507e164
Daniel Silva <daniel.raizon@gmail.com>
2021-09-27 04:04:30 -0300

    Set toast messages.

--- React/react-base/src/App.js
+++ React/react-base/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 
 import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
@@ -7,11 +8,15 @@ import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
+	toast.success('Sucesso!', { autoClose: 3000, toastId: 'success' });
+	toast.error('Deu ruim!', { autoClose: 3000, toastId: 'error' });
+
 	return (
 		<Router history={history}>
 			<Header />
 			<Routes />
 			<GlobalStyles />
+			<ToastContainer className='toast-container' />
 		</Router>
 	);
 }
